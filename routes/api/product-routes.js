dfc5bc977999021718f61c35069f5f6db98685b6
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-07-05 19:22:54 -0700

    Product delete by id route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -131,8 +131,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete product by its `id`
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
