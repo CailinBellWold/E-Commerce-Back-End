e5c64fb7bf5ed263f724ef22acf64d72193d10c7
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-07-05 19:18:28 -0700

    Tag delete by id route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,8 +63,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete tag by its `id`
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
